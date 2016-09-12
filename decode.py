d30936a54de874400d8966f620d42882dc6d5bd2
ETumang <emily.tumang@gmail.com>
2016-09-12 02:06:25 -0400

    Fixed import errors and disabled filter. Note; this still isn't fully functional. Functional up to correlation step.

--- decode.py
+++ decode.py
@@ -1,30 +1,40 @@
 import wave
 from scipy import signal
+from scipy.io.wavfile import read
+from matplotlib import pyplot as plt
+import numpy as np
 
 RATE = 144000
 
+wav = read("data.wav")
 
-filename = raw_input('Enter the file path to be interpreted.')
+#filename = raw_input('Enter the file path to be interpreted.')
+#wav = read(filename)
 
-data = wave.open(filename)
+data = np.array(wav[1],dtype=float)
 
-filter,a = signal.butter(9,[1000/(RATE/2)],'low')
+d = np.linspace(0,1,5000)
+
+filt,a = signal.butter(9,[1000/(RATE/2)],'low')
 
 data = abs(data)
 
-offset = mean(data)
+offset = np.mean(data)
 
-data = signal.filtfilt(filter,a,data)
+#data = signal.filtfilt(filt,a,data)
 
-data = data - offset
+#data = data - offset
 
-t = [0:1/RATE:1/160]
+norm = max(data)
 
-sync_wave = signal.square(1040*t*(2*pi))
+data = data/norm
+
+t = np.linspace(0,1,data.shape[0])
+
+sync_wave = signal.square(t*(2*np.pi))
 
 sync_res = signal.correlate(sync_wave, data)
 
-sync_res = sync_res[:length(data)]
-
-
+plt.plot(t,sync_res[0:5000])
+plt.show()
 
